55b28c60489180d9079806a0eb70b80d08441b54
zhuzhengjian <aibyanyu@qq.com>
2021-12-05 13:36:55 +0800

    fix: 修改信息

--- plugins/demoBlock/component/demo/index.ts
+++
@@ -1,9 +0,0 @@
-import DemoBlock from "./demo.vue"
-import { App,Plugin } from "vue"
-export { DemoBlock };
-
-export default {
-    install(app:App){
-        app.component('demo',DemoBlock);
-    }
-} as Plugin;
