f99d3b9d454fe538cce430ce0df8f4b54bb35c3e
aibayanyu20 <aibayanyu@qq.com>
2022-11-22 16:12:24 +0800

    fix: change url error

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { fileURLToPath } from 'url'
 import type { PluginOption, ResolvedConfig } from 'vite'
 import type { MarkdownRenderer } from 'vitepress'
 import { createMarkdownRenderer } from 'vitepress'
 import { normalizePath } from 'vite'
 import type { UserOptions } from './typing'
 import { Parser } from './parser'
-const themePath = fileURLToPath(new URL('./theme', import.meta.url))
 
 const vitePluginVitepressDemo = (_opt?: UserOptions): PluginOption => {
   let config: ResolvedConfig
@@ -24,7 +22,6 @@ const vitePluginVitepressDemo = (_opt?: UserOptions): PluginOption => {
         resolve: {
           alias: {
             [aliasName]: path,
-            '/@/VITEPRESS_THEME': themePath,
           },
         },
         ssr: {
